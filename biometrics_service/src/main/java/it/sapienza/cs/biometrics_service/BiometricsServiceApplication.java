505948a37b17dac6499dbf0fd0d5e9da8a1bcce1
francescobianca <francybi1996@gmail.com>
2020-01-30 18:53:05 +0100

    First Commit
    Setup of the application environment

---
+++ biometrics_service/src/main/java/it/sapienza/cs/biometrics_service/BiometricsServiceApplication.java
@@ -0,0 +1,13 @@
+package it.sapienza.cs.biometrics_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BiometricsServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BiometricsServiceApplication.class, args);
+	}
+
+}
---
+++ biometrics_service/src/test/java/it/sapienza/cs/biometrics_service/BiometricsServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package it.sapienza.cs.biometrics_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BiometricsServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
