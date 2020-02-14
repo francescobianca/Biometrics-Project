6bdcdc6144af71c3db53fc7661b4dae740b0d281
Andrea Magnante <32498316+andreamagnante@users.noreply.github.com>
2020-02-14 14:04:44 +0100

    Add files via upload

--- Face_Recognition/evaluation.py
+++ Face_Recognition/evaluation.py
@@ -2,90 +2,77 @@ import face_recognition
 import pickle
 import cv2
 import numpy as np
+import PySimpleGUI as sg
 
-# Get a reference to webcam #0 (the default one)
-video_capture = cv2.VideoCapture(0)
+def startEvaluation():
+    video_capture = cv2.VideoCapture(0)
 
-# load the known faces and embeddings
-print("[INFO] loading encodings...")
-data = pickle.loads(open("encodings", "rb").read())
+    print("[INFO] loading encodings...")
+    data = pickle.loads(open("encodings", "rb").read())
 
-#data = {"encodings": knownEncodings, "names": knownNames}
+    face_locations = []
+    face_encodings = []
+    face_names = []
+    process_this_frame = True
+    attendances = {}
 
-# Initialize some variables
-face_locations = []
-face_encodings = []
-face_names = []
-process_this_frame = True
-attendances = {}
+    while True:
 
-while True:
-    # Grab a single frame of video
-    ret, frame = video_capture.read()
+        ret, frame = video_capture.read()
 
-    # Resize frame of video to 1/4 size for faster face recognition processing
-    small_frame = cv2.resize(frame, (0, 0), fx=0.25, fy=0.25)
+        small_frame = cv2.resize(frame, (0, 0), fx=0.25, fy=0.25)
 
-    # Convert the image from BGR color (which OpenCV uses) to RGB color (which face_recognition uses)
-    rgb_small_frame = small_frame[:, :, ::-1]
+        rgb_small_frame = small_frame[:, :, ::-1]
 
-    # Only process every other frame of video to save time
-    if process_this_frame:
-        # Find all the faces and face encodings in the current frame of video
-        face_locations = face_recognition.face_locations(rgb_small_frame)
-        face_encodings = face_recognition.face_encodings(rgb_small_frame, face_locations)
+        if process_this_frame:
 
-        face_names = []
-        for face_encoding in face_encodings:
-            # See if the face is a match for the known face(s)
-            matches = face_recognition.compare_faces(data["encodings"], face_encoding)
-            name = "Unknown"
+            face_locations = face_recognition.face_locations(rgb_small_frame)
+            face_encodings = face_recognition.face_encodings(rgb_small_frame, face_locations)
 
-            # # If a match was found in known_face_encodings, just use the first one.
-            # if True in matches:
-            #     first_match_index = matches.index(True)
-            #     name = known_face_names[first_match_index]
+            face_names = []
+            for face_encoding in face_encodings:
 
-            # Or instead, use the known face with the smallest distance to the new face
-            face_distances = face_recognition.face_distance(data["encodings"], face_encoding)
+                matches = face_recognition.compare_faces(data["encodings"], face_encoding, tolerance=0.6)
 
-            best_match_index = np.argmin(face_distances)
+                name = "Unknown"
 
-            if matches[best_match_index]:
-                name = data["names"][best_match_index]
-                attendances[name] = 1
+                face_distances = face_recognition.face_distance(data["encodings"], face_encoding)
+                best_match_index = np.argmin(face_distances)
 
-            face_names.append(name)
+                confidence = face_distances[best_match_index]
 
-            #for key,value in attendances.items():
-                #print("Presenze: " + key)
+                if matches[best_match_index]:
+                    name = data["names"][best_match_index]
+                    print(name)
+                    r = attendances.get(name, None)
+                    if r == None:
+                        attendances[name] = confidence
+                    elif attendances[name] > confidence: 
+                        attendances[name] = confidence
+                    print(attendances[name])
 
-    process_this_frame = not process_this_frame
+                face_names.append(name)
 
+                
+        process_this_frame = not process_this_frame
 
-    # Display the results
-    for (top, right, bottom, left), name in zip(face_locations, face_names):
-        # Scale back up face locations since the frame we detected in was scaled to 1/4 size
-        top *= 4
-        right *= 4
-        bottom *= 4
-        left *= 4
+        for (top, right, bottom, left), name in zip(face_locations, face_names):
+            top *= 4
+            right *= 4
+            bottom *= 4
+            left *= 4
 
-        # Draw a box around the face
-        cv2.rectangle(frame, (left, top), (right, bottom), (0, 0, 255), 2)
+            cv2.rectangle(frame, (left, top), (right, bottom), (0, 0, 255), 2)
 
-        # Draw a label with a name below the face
-        cv2.rectangle(frame, (left, bottom - 35), (right, bottom), (0, 0, 255), cv2.FILLED)
-        font = cv2.FONT_HERSHEY_DUPLEX
-        cv2.putText(frame, name, (left + 6, bottom - 6), font, 1.0, (255, 255, 255), 1)
+            cv2.rectangle(frame, (left, bottom - 35), (right, bottom), (0, 0, 255), cv2.FILLED)
+            font = cv2.FONT_HERSHEY_DUPLEX
+            cv2.putText(frame, name, (left + 6, bottom - 6), font, 1.0, (255, 255, 255), 1)
 
-    # Display the resulting image
-    cv2.imshow('Video', frame)
+        cv2.imshow('Video', frame)
 
-    # Hit 'q' on the keyboard to quit!
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
 
-# Release handle to the webcam
-video_capture.release()
-cv2.destroyAllWindows()
+    video_capture.release()
+    cv2.destroyAllWindows()
+    return attendances
